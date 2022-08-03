2237c7e9f917ef9ba6e1318836be011632993e81
redbor24 <redbor24@gmail.com>
2022-08-03 17:39:34 +0300

    Админка для Мероприятия, Детали мероприятия и Участник

--- event/admin.py
+++ event/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from event.models import Participant, Meetup, MeetupDetail
+from django.utils.html import format_html
 
-# Register your models here.
+
+class MeetupDetailInline(admin.StackedInline):
+    model = MeetupDetail
+    extra = 0
+
+
+@admin.register(Meetup)
+class MeetupAdmin(admin.ModelAdmin):
+    inlines = [MeetupDetailInline]
+    save_on_top = True
+
+
+@admin.register(Participant)
+class ParticipantAdmin(admin.ModelAdmin):
+    fields = ('preview_image', 'fio', 'telegram_id', 'email', 'phone', 'company', 'position', 'image')
+    readonly_fields = ['preview_image']
+    save_on_top = True
+
+    def preview_image(self, obj):
+        return format_html('<img src="{}" height={} />', obj.image.url, 200)
