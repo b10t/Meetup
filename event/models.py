dcd8708c124098545bfb9d80a5706c72f2b637a9
redbor24 <redbor24@gmail.com>
2022-08-04 15:02:00 +0300

    Добавлена моедль "Вопросы докладчику" и часть админки для неё

--- event/models.py
+++ event/models.py
@@ -3,6 +3,9 @@ from phonenumber_field.modelfields import PhoneNumberField
 from tinymce.models import HTMLField
 
 
+LISTENER = 'listener'
+SPEAKER = 'speaker'
+
 class Participant(models.Model):
     fio = models.CharField(max_length=255, verbose_name='ФИО', db_index=True)
     telegram_id = models.BigIntegerField(verbose_name='Телеграм ID', unique=True, db_index=True)
@@ -56,10 +59,15 @@ class Event(models.Model):
         return f'{self.meetup} - {self.name}'
 
 
+class EventParticipantQuerySet(models.QuerySet):
+    def get_speakers(self):
+        return self.filter(status='speaker')
+
+
 class EventParticipant(models.Model):
     PARTICIPANT_STATUS = (
-        ('listener', 'слушатель'),
-        ('speaker', 'докладчик')
+        (LISTENER, 'слушатель'),
+        (SPEAKER, 'докладчик')
     )
     event = models.ForeignKey(Event, verbose_name='Событие', on_delete=models.CASCADE,
                               related_name='participants')
@@ -67,10 +75,31 @@ class EventParticipant(models.Model):
                                     related_name='events')
     status = models.CharField(max_length=10, verbose_name='Статус участника', choices=PARTICIPANT_STATUS, db_index=True)
 
+    objects = EventParticipantQuerySet.as_manager()
+
     class Meta:
         verbose_name = 'Участник события'
         verbose_name_plural = 'Участники событий'
 
-    @property
     def __str__(self):
         return f'{self.event} - {self.participant} ({self.get_status_display()})'
+
+
+class Question(models.Model):
+    speaker = models.ForeignKey(EventParticipant, related_name='speakers', verbose_name='Докладчики на событии',
+                                on_delete=models.CASCADE)
+    asker = models.ForeignKey(EventParticipant, related_name='askers', verbose_name='Кто задал вопрос',
+                              on_delete=models.CASCADE)
+    question = models.TextField(verbose_name='Текст вопроса')
+
+    class Meta:
+        verbose_name = 'Вопрос докладчику'
+        verbose_name_plural = 'Вопросы докладчику'
+
+    def short_question(self):
+        return self.question[:100]
+    short_question.short_description = 'Начало текста вопроса'
+
+    def __str__(self):
+        speaker = self.speaker
+        return f'{speaker.event}, {speaker.participant} - {self.asker.participant}'
