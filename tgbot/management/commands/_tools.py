8b6047c06525ea535484337d1ffa757297977249
b10t <b10t.rus@gmail.com>
2022-08-06 19:24:08 +0700

    Удалил лишнию функцию get_meetups.

--- tgbot/management/commands/_tools.py
+++ tgbot/management/commands/_tools.py
@@ -1,11 +1,6 @@
 from event.models import Meetup, Event
 
 
-def get_meetups():
-    meetups = Meetup.objects.all()
-    return [meetup for meetup in meetups]
-
-
 def get_event(meetup_id):
     events = Event.objects.filter(meetup_id=meetup_id)
     return [event for event in events]
